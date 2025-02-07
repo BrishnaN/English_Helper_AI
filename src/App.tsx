bf5efd8cda8d7677d5bccbcb01229ac216f0f49e
BrishnaN <brishna@udel.edu>
2025-02-07 14:40:34 -0500

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -34,57 +34,68 @@ const App: React.FC = () => {
 
   return (
     <Router>
-      <div className="body">
-        {/* Navigation */}
-        <nav>
-          <Link to="/">Home</Link> | <Link to="/signup">Sign Up</Link> |{" "}
-          <Link to="/login">Login</Link>
-        </nav>
+      <Routes>
+        <div className="body">
+          {/* Navigation */}
+          <nav>
+            <Link to="/">Home</Link> | <Link to="/signup">Sign Up</Link> |{" "}
+            <Link to="/login">Login</Link>
+          </nav>
 
-        {/* Routes */}
+          {/* Routes */}
 
-        <Routes>
-          <Route path="English_Helper_AI/" element={<Home />} />
-          <Route path="English_Helper_AI/signup" element={<SignUp />} />
-          <Route path="English_Helper_AI/login" element={<Login />} />
-          <Route
-            path="English_Helper_AI/assessmentPage"
-            element={<AssessmentPage />}
-          />
-          <Route path="English_Helper_AI/assessment" element={<Assessment />} />
-          <Route
-            path="English_Helper_AI/forgot-password"
-            element={<ForgotPassword />}
-          />
-          <Route
-            path="English_Helper_AI/beginnerPage"
-            element={<BeginnerPage />}
-          />
-          <Route
-            path="English_Helper_AI/lettersPage"
-            element={<LettersPage />}
-          />
-          <Route
-            path="English_Helper_AI/numbersPage"
-            element={<NumbersPage />}
-          />
-          <Route path="English_Helper_AI/familyPage" element={<FamilyPage />} />
-          <Route path="English_Helper_AI/colorsPage" element={<ColorsPage />} />
-        </Routes>
+          <Routes>
+            <Route path="English_Helper_AI/" element={<Home />} />
+            <Route path="English_Helper_AI/signup" element={<SignUp />} />
+            <Route path="English_Helper_AI/login" element={<Login />} />
+            <Route
+              path="English_Helper_AI/assessmentPage"
+              element={<AssessmentPage />}
+            />
+            <Route
+              path="English_Helper_AI/assessment"
+              element={<Assessment />}
+            />
+            <Route
+              path="English_Helper_AI/forgot-password"
+              element={<ForgotPassword />}
+            />
+            <Route
+              path="English_Helper_AI/beginnerPage"
+              element={<BeginnerPage />}
+            />
+            <Route
+              path="English_Helper_AI/lettersPage"
+              element={<LettersPage />}
+            />
+            <Route
+              path="English_Helper_AI/numbersPage"
+              element={<NumbersPage />}
+            />
+            <Route
+              path="English_Helper_AI/familyPage"
+              element={<FamilyPage />}
+            />
+            <Route
+              path="English_Helper_AI/colorsPage"
+              element={<ColorsPage />}
+            />
+          </Routes>
 
-        {/* API Key Form */}
-        <div className="form-container">
-          <Form.Control
-            type="password"
-            placeholder="Insert API Key Here"
-            onChange={changeKey}
-            style={{ width: "300px" }}
-          />
-          <Button className="Submit-Button" onClick={handleSubmit}>
-            Submit
-          </Button>
+          {/* API Key Form */}
+          <div className="form-container">
+            <Form.Control
+              type="password"
+              placeholder="Insert API Key Here"
+              onChange={changeKey}
+              style={{ width: "300px" }}
+            />
+            <Button className="Submit-Button" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </div>
         </div>
-      </div>
+      </Routes>
     </Router>
   );
 };
