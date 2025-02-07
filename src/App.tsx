b8d2c778b46576adb28521fface811809a3525ab
BrishnaN <brishna@udel.edu>
2025-02-07 14:30:59 -0500

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Button, Form } from "react-bootstrap";
 import Home from "./components/Home.tsx";
 import Assessment from "./components/Assessment.tsx";
@@ -13,6 +13,7 @@ import NumbersPage from "./components/NumbersPage.tsx";
 import FamilyPage from "./components/FamilyPage.tsx";
 import ColorsPage from "./components/ColorsPage.tsx";
 import "./App.css";
+import { useNavigate } from "react-router-dom";
 //import "./styles.css";
 
 const saveKeyData = "MYKEY";
@@ -32,41 +33,59 @@ const App: React.FC = () => {
   }
 
   return (
-    <div className="body">
-      {/* Navigation */}
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/signup">Sign Up</Link> |{" "}
-        <Link to="/login">Login</Link>
-      </nav>
+    <Router>
+      <div className="body">
+        {/* Navigation */}
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/signup">Sign Up</Link> |{" "}
+          <Link to="/login">Login</Link>
+        </nav>
 
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/assessmentPage" element={<AssessmentPage />} />
-        <Route path="/assessment" element={<Assessment />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/beginnerPage" element={<BeginnerPage />} />
-        <Route path="/lettersPage" element={<LettersPage />} />
-        <Route path="/numbersPage" element={<NumbersPage />} />
-        <Route path="/familyPage" element={<FamilyPage />} />
-        <Route path="/colorsPage" element={<ColorsPage />} />
-      </Routes>
+        {/* Routes */}
 
-      {/* API Key Form */}
-      <div className="form-container">
-        <Form.Control
-          type="password"
-          placeholder="Insert API Key Here"
-          onChange={changeKey}
-          style={{ width: "300px" }}
-        />
-        <Button className="Submit-Button" onClick={handleSubmit}>
-          Submit
-        </Button>
+        <Routes>
+          <Route path="English_Helper_AI/" element={<Home />} />
+          <Route path="English_Helper_AI/signup" element={<SignUp />} />
+          <Route path="English_Helper_AI/login" element={<Login />} />
+          <Route
+            path="English_Helper_AI/assessmentPage"
+            element={<AssessmentPage />}
+          />
+          <Route path="English_Helper_AI/assessment" element={<Assessment />} />
+          <Route
+            path="English_Helper_AI/forgot-password"
+            element={<ForgotPassword />}
+          />
+          <Route
+            path="English_Helper_AI/beginnerPage"
+            element={<BeginnerPage />}
+          />
+          <Route
+            path="English_Helper_AI/lettersPage"
+            element={<LettersPage />}
+          />
+          <Route
+            path="English_Helper_AI/numbersPage"
+            element={<NumbersPage />}
+          />
+          <Route path="English_Helper_AI/familyPage" element={<FamilyPage />} />
+          <Route path="English_Helper_AI/colorsPage" element={<ColorsPage />} />
+        </Routes>
+
+        {/* API Key Form */}
+        <div className="form-container">
+          <Form.Control
+            type="password"
+            placeholder="Insert API Key Here"
+            onChange={changeKey}
+            style={{ width: "300px" }}
+          />
+          <Button className="Submit-Button" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
